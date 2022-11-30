15ed3e0c10d99d0122f129c63575e2ea213da7c3
Bellmere <therise00@gmail.com>
2022-11-30 19:50:14 +0200

    Update refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export default function getRefs() {
+    return {
+        formEl: document.querySelector('.search-form'),
+        searchInput: document.querySelector('.search-form-input'),
+        searchBtn: document.querySelector('.search-form-button'),
+        galleryEl: document.querySelector('.gallery'),
+
+    }
+}
