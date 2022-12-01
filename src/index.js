d705dba0b22657fb55c25f6466588bf4e0878606
Bellmere <therise00@gmail.com>
2022-12-01 09:24:01 +0200

    add simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import getRefs from './js/refs';
 import Notiflix from 'notiflix';
 import { fetchImages } from './js/fetcImages';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = getRefs();
 
