1d2f41394ec725c312ac57385cd6ca7e9e997313
Bellmere <therise00@gmail.com>
2022-12-07 23:24:20 +0200

    span message

--- src/index.js
+++ src/index.js
@@ -49,11 +49,7 @@ function onSearchBtn(e) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    } else if (!r.data.hits.length) {
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    } 
+    }
      else {
       renderImages(r.data.hits);
       Notiflix.Notify.success(`Hooray! We found ${r.data.totalHits} images.`);
